000490ea1e69f6ed87a9b6b60445df5ed730d514
Yuvisa QP <yuvisa.palomino@gmail.com>
2022-09-15 15:38:49 -0500

    added the service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { EjemploLeafletComponent } from './ejemplo-leaflet/ejemplo-leaflet.component';
 
@@ -10,7 +10,8 @@ import { EjemploLeafletComponent } from './ejemplo-leaflet/ejemplo-leaflet.compo
     EjemploLeafletComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/leaflet.service.ts
+++ src/app/services/leaflet.service.ts
@@ -1,19 +1,24 @@
-import * as L from 'leaflet';
-import 'leaflet-routing-machine';
-import { icon,Marker } from 'leaflet';
-import { Injectable,Input,OnInit } from '@angular/core';
 
-export const DEFAULT_LAT = 48.20807;
-export const DEFAULT_LON =  16.37320;
-export const TITULO = 'Proyecto';
-const iconRetinaUrl = 'assets/marker-icon-2x.png';
-const iconUrl = 'assets/marker-icon.png';
-const shadowUrl = 'assets/marker-shadow.png';
+import { Injectable,Input,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeafletService {
+    /**Nombre de la bd */
+    bd_Couch:string= "visitas_medicas";
+     /**ACCESO A BASE DE DATOS COUCHDB */
+    base_urlCouch:string = "http://192.168.5.3:5984";
+    token:string="eyJraWQiOiJmb28iLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY2MzMxMDIxNX0.FLWMevT9S0QqWcdFds5j4XDmQ3bjHXcYIngSxc4VtPA";
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  mostrarVisitas():Observable <any>{
+    return this.http.post <any []> (`http://192.168.5.3:5984/visitas_medicas/_design/visita_domiciliaria/_view/visita_domiciliaria_por_profesional_v1`, {
+        "keys": ["vp72753957"]
+      },{headers:{'Authorization':`Bearer ${this.token}`}}
+  );
+  }
+
 }
